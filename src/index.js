4ab56eb238be2e11098f3f4e92ddbb295e95f170
lizhigaomac <lizhigao@021.com>
2018-08-05 19:54:32 +0800

    finished

--- src/index.js
+++ src/index.js
@@ -52,11 +52,12 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -69,21 +70,29 @@ class Game extends React.Component {
           squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+  jumpTo(move) {
+    this.setState({
+      stepNumber: move,
+      xIsNext: move % 2 === 0
     });
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    // const moves = history.map((move, step) => {
-    //   return (
-    //     <li key={step}>
-    //       <button>{step ? `回到第${step}步` : '回到起始位置'}</button>
-    //     </li>
-    //   );
-    // });
+    const moves = history.map((step, move) => {
+      const desc = move ? `回到第${move}步` : '回到起始位置';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -103,7 +112,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          {/* <ol>{moves}</ol> */}
+          <ol>{moves}</ol>
         </div>
       </div>
     );
